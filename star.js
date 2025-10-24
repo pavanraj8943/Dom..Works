7d9da5c25315db03ee6b16f2d19bf6dc066ef0e7
pavanraj8943 <pavan.64j@gmail.com>
2025-10-24 17:19:23 +0530

    star peramid print

--- star.js
+++ star.js
@@ -1,16 +1,19 @@
 document.getElementById("multi").addEventListener('click', () => {
     let x = parseInt(document.getElementById("num").value)
-   
-    for(i=1;i<=x;i++){
-        str='';
-        for(j=i;j<x;j++){
-            str+=" "   
-        } 
-        for(k=1;k<=i;k++){
-            str+="* "
-        } 
-        console.log(str);
-        
+
+if (x>=50)
+   return alert("Kauthukam lesham kooduthalaa alle....🤫")
+    let inner = ""
+    for (i = 1; i <= x; i++) {
+        str = '';
+        for (j = i; j < x; j++) {
+            str += " "
+        }
+        for (k = 1; k <= i; k++) {
+            str += "* "
+        }
+        inner += str + "\n"
+
     }
-    document.getElementById("res").innerHTML=str
-})
+    document.getElementById("res").innerHTML = `<pre>${inner}</pre>`
+});
