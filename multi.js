02e0ca566ce77fe0ef59119ac651d85935648c89
pavanraj8943 <pavan.64j@gmail.com>
2025-10-24 14:59:33 +0530

    multiple table added

---
+++ multi.js
@@ -0,0 +1,11 @@
+
+document.getElementById("multi").addEventListener('click', () => {
+    let x = document.getElementById("num").value
+    let y = 10;
+    str = ''
+    for (let i = 1; i <= y; i++) { 
+        str+=`<p>${i} * ${x} = ${x * i}</p> `
+    }
+
+    document.getElementById("res").innerHTML = str
+})
