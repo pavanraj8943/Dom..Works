4a46fa1a2f4eb59b8c701ddb4e329b9a22ad7dff
pavanraj8943 <pavan.64j@gmail.com>
2025-10-27 10:15:38 +0530

    todo added

--- todo.js
+++ todo.js
@@ -3,30 +3,34 @@ let todo = [];
 document.getElementById("add").addEventListener("click", () => {
     let values = document.getElementById("task").value;
 
-    if (values == " ")
+    if (values == "") {
         return alert("Enter a task Name")
-    todo.push(values)
 
+    }
+    todo.push(values)
+    document.getElementById("task").value = ""
+    displayTask()
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// str = '';
-// str += `<p>${values} <button id="delete">Delete</button></p>`
-// document.getElementById("out").innerHTML = str
+function displayTask() {
+    let str = ``;
+    for (const [index, task] of todo.entries()) {
+        str += `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${task}</td>
+            <td><button id="edit">Edit</button></td>
+            <td><button id="delete" onclick="deletes(${index})">Delete</button></td>
+        </tr>
+        `
+    }
+    document.getElementById("display").innerHTML = str
+}
+function deletes(index) {
+    let tasks = todo[index];
+    if (confirm(`Are you sure "${index + 1},${tasks}" want to delete?`)) {
+        todo.splice(index,1);
+        displayTask()
+    }
+}
